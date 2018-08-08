2b4ed6b8bc436fbce626aea311f1b756b49ecc07
Yuriy Lazarev <Unisay@users.noreply.github.com>
2018-08-08 14:12:02 +0200

    notImplemented

--- src/Preamble.purs
+++ src/Preamble.purs
@@ -1,12 +1,17 @@
 module Preamble
   ( module Prelude
   , fail
+  , notImplemented
   ) where
 
 import Prelude
 
 import Control.Monad.Error.Class (class MonadThrow)
 import Effect.Aff (Error, error, throwError)
+import Unsafe.Coerce (unsafeCoerce)
 
 fail :: ∀ m a . MonadThrow Error m => String -> m a
 fail = throwError <<< error
+
+notImplemented :: ∀ a . a
+notImplemented = unsafeCoerce "Not implemented"
