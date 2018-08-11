f026e2f9e8f2562599abf3c9e6c8076277db52f7
Yuriy Lazarev <Unisay@users.noreply.github.com>
2018-08-11 14:21:46 +0200

    Re-export Either, Maybe

--- src/Preamble.purs
+++ src/Preamble.purs
@@ -1,5 +1,8 @@
 module Preamble
   ( module Prelude
+  , module DE
+  , module DM
+  , type (\/)
   , fail
   , notImplemented
   ) where
@@ -7,8 +10,12 @@ module Preamble
 import Prelude
 
 import Control.Monad.Error.Class (class MonadThrow)
+import Data.Either (Either(..), choose, either, fromLeft, fromRight, hush, isLeft, isRight, note, note') as DE
+import Data.Maybe (Maybe(..), fromJust, fromMaybe, fromMaybe', isJust, isNothing, maybe, maybe', optional) as DM
 import Effect.Aff (Error, error, throwError)
 import Unsafe.Coerce (unsafeCoerce)
+
+infixr 6 type DE.Either as \/
 
 fail :: ∀ m a . MonadThrow Error m => String -> m a
 fail = throwError <<< error
